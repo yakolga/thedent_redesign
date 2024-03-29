0013969eba27b3c34af4300e5c433ef88b88567a
Olga Kulczycka <olga.kulczycka07@gmail.com>
2024-03-29 22:42:24 +0200

    feat: code services section & doctors section & reviews section

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,9 @@
 
 toggleMobileMenu();
 closeMobile();
+hideServices();
+slickDoctorSection();
+slickReviewsSection();
 
 function toggleMobileMenu() {
   const buttonToggle = document.querySelector('.header__burger'),
@@ -25,4 +28,88 @@ function closeMobile() {
   buttonClose.addEventListener('click', () => {
     mobileMenu.classList.remove('--active');
   });
+}
+
+function hideServices() {
+  if (screen.width <= 757) {
+    const servicesWrapper = document.querySelector('.services__wrapper'),
+          services = servicesWrapper.querySelectorAll('.services__service');
+
+    for (let i = 0; i < services.length; i++) {
+      if (i > 3) {
+        services[i].classList.add('d-none');
+        services[i].setAttribute('data-hidden', true);
+      }
+    }
+
+    const hiddenServices = document.querySelectorAll('.services__service[data-hidden]');
+
+    const showButton = document.createElement('button');
+    showButton.classList.add('services__button', 'btn');
+    showButton.id = 'show-button';
+    showButton.innerText = 'Більше послуг';
+    servicesWrapper.after(showButton);
+
+    const hideButton = document.createElement('button');
+    hideButton.classList.add('services__button', 'btn', 'd-none');
+    hideButton.id = 'hide-button';
+    hideButton.innerText = 'Менше послуг';
+    servicesWrapper.after(hideButton);
+
+    showButton.addEventListener('click', (e) => {
+      hiddenServices.forEach(service => {
+        service.classList.remove('d-none');
+        e.target.classList.add('d-none');
+        hideButton.classList.remove('d-none');
+      })
+    });
+
+    hideButton.addEventListener('click', (e) => {
+      hiddenServices.forEach(service => {
+        service.classList.add('d-none');
+        e.target.classList.add('d-none');
+        showButton.classList.remove('d-none');
+      })
+    });
+  }
+}
+
+function slickDoctorSection() {
+  if (screen.width > 977) {
+    $('.doctors__wrapper').slick({
+      infinite: true,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      prevArrow: '<button class="slider-arrow doctors__prev"><i class="icon-arrow-left"></i></button>',
+      nextArrow: '<button class="slider-arrow doctors__next"><i class="icon-arrow-right"></i></button>',
+      responsive: [
+        {
+          breakpoint: 977,
+          settings: {
+            slidesToShow: 3,
+          }
+        },
+      ]
+    });
+  }
+}
+
+function slickReviewsSection() {
+  if (screen.width > 977) {
+    $('.reviews__wrapper').slick({
+      infinite: true,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      prevArrow: '<button class="slider-arrow reviews__prev"><i class="icon-arrow-left"></i></button>',
+      nextArrow: '<button class="slider-arrow reviews__next"><i class="icon-arrow-right"></i></button>',
+      responsive: [
+        {
+          breakpoint: 977,
+          settings: {
+            slidesToShow: 3,
+          }
+        },
+      ]
+    });
+  }
 }
